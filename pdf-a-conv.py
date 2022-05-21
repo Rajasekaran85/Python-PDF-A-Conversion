94def9937985e31547c6fc47a411af6c0bd74268
Rajasekaran85 <rajasekaran.al@gmail.com>
2022-05-21 15:36:19 +0530

    Add files via upload
    PDF to PDF/A - type 1b Conversion

---
+++ pdf-a-conv.py
@@ -0,0 +1,40 @@
+import sys
+import os
+import shutil
+import ghostscript
+
+print("\n PDF to PDF/A Conversion \n")
+print("\n Developed by A Rajasekaran\n")
+print("\n Date: 20 April 2022 \n\n")
+
+
+filepath1 = input(" Enter the File path: ")
+
+Output1 = input(" Enter the Output path: ")
+
+output = Output1 + "\\"
+
+filepath = filepath1 + "\\"
+
+filelist = os.path.isdir(filepath) # specified path is an existing directory or not
+
+
+
+
+# os.listdir = list the file list in the directory
+for fname in os.listdir(filepath):
+    print(fname)
+    if not fname.endswith(".pdf"):
+        continue
+    path = os.path.join(filepath, fname) 
+    print(path)
+    
+    #print(fname)
+    #print(path)
+    ghostScriptExec = ['gs', '-dPDFA', '-dBATCH', '-dNOPAUSE', '-dUseCIEColor', '-sProcessColorModel=DeviceRGB',
+                   '-sDEVICE=pdfwrite', '-dFastWebView', '-sPDFACompatibilityPolicy=1',
+                   '-sOutputFile='+ output + 'PDFA-' + fname, path]
+    ghostscript.Ghostscript(*ghostScriptExec)
+    #print(filepath)
+print("\n Conversion Completed\n")
+
---
+++ pdf-a-conv_GUI.py
@@ -0,0 +1,64 @@
+from tkinter import filedialog
+from tkinter import *
+import sys
+import os
+import shutil
+from PIL import Image
+import io
+import pathlib
+import ghostscript
+
+
+def browse_button():
+    # Allow user to select a directory and store it in global var
+    # called folder_path
+    global folder_path
+    global filename
+    filename = filedialog.askdirectory()
+    folder_path.set(filename)
+    #print(folder_path)
+    #print(filename)
+
+def conv():
+    print("\n PDF to PDF/A Conversion \n")
+    print("\n Developed by A Rajasekaran\n")
+    print("\n Date: 20 April 2022 \n\n")
+    #f1 = os.listdir(filename) 
+    for fname in os.listdir(filename):
+        print(fname)
+        if not fname.endswith(".pdf"):
+            #print(fname)
+            continue
+        path = os.path.join(filename, fname)
+        #test = os.path.splitext(fname)[0]
+        #print(test)
+        value1 = filename + '/' + fname
+        value2 = filename + '/'
+        print(value1)
+        ghostScriptExec = ['gs', '-dPDFA', '-dBATCH', '-dNOPAUSE', '-dUseCIEColor', '-sProcessColorModel=DeviceRGB',
+                   '-sDEVICE=pdfwrite', '-dFastWebView', '-sPDFACompatibilityPolicy=1',
+                   '-sOutputFile='+ value2 + 'PDFA-' + fname, path]
+        ghostscript.Ghostscript(*ghostScriptExec)
+    messagebox.showinfo("PDF/A Conversion", "Completed")
+    print(filename)
+    print(fname)
+    print(value1)
+
+root = Tk()
+root.geometry("400x250")
+root.config(bg='#ffcc00') 
+
+folder_path = StringVar()
+label_value = 'PDF to PDF/A Conversion'
+lbl1 = Label(root, text='PDF to PDF/A Conversion', font='helvetica 15', bg='#ffcc00')
+lbl1.pack(pady=10)
+button2 = Button(text="Browse", command=browse_button, bg='royalblue', fg = 'white').pack(pady=25)
+button3 = Button(text="Submit", command=conv, bg='royalblue', fg = 'white').pack(pady=25)
+lbl2 = Label(root, textvariable=folder_path, font='helvetica 12', bg='#ffcc00')
+lbl2.pack(pady=10)
+
+
+
+mainloop()
+
+
